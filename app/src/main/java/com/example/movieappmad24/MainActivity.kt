55d30f1be9cb7d524ff13cd92c4dffd591d9afc3
Numan52 <numan.demir@gmx.at>
2024-03-10 18:45:09 +0100

    dasda

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -1,35 +1,278 @@
 package com.example.movieappmad24
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.movieappmad24.models.Movie
+import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
+
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
             MovieAppMAD24Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Card {
-                        Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
-                    }
+                    MovieAppContent(getMovies())
                 }
             }
         }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MovieAppContent(movieList: List<Movie>) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopAppBar() },
+        bottomBar = { BottomAppBar() }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 70.dp, bottom = 70.dp)
+        ) {
+            MovieList(getMovies())
+        }
+    }
+}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun BottomAppBar() {
+    BottomAppBar(
+        modifier = Modifier.fillMaxWidth(),
+        content = {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .padding(0.dp)
+                ){
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Home,
+                            contentDescription = "Home",
+                            tint = Color.Black
+                        )
+                    }
+                    Text(
+                        text = "Home",
+                        color = Color.Black)
+                }
+                Spacer(modifier = Modifier.width(130.dp))
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = "Watchlist",
+                            tint = Color.Black
+                        )
+                    }
+                    Text(text = "Watchlist")
+
+                }
+
+            }
+        }
+    )
+}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar() {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color(106, 235, 205, 255),
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text(
+                "Movie App",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center)
+        }
+    )
+
+}
+
+
+@Composable
+fun MovieList(movieList: List<Movie>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(movieList) { movie ->
+            MovieRow(movie = movie)
+        }
+    }
+}
+
+
+@Composable
+fun MovieRow(movie: Movie) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(6.dp)
+            .clip(shape = RoundedCornerShape(20.dp)),
+        Arrangement.Center,
+        Alignment.CenterHorizontally
+    ) {
+
+        AsyncImage(
+            model = movie.images[0],
+            contentDescription = "Movie Image",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
+        ExpandableMovieDetails(movie = movie)
+
+    }
+}
+
+@Composable
+fun ExpandableMovieDetails(movie: Movie) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+            ),
+        shape = RoundedCornerShape(4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(
+                    text = movie.title,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(6.dp)
+
+                )
+                
+                Icon(
+                    imageVector = if (expanded) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp,
+                    contentDescription = "KeyboardArrowUp",
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .clickable {
+                            expanded = !expanded
+                        }
+
+                )
+            }
+            if (expanded) {
+                MovieDetails(movie)
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieDetails(movie: Movie) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp)
+    ) {
+        Text("Director: ${movie.director}")
+        Text("Released: ${movie.year}")
+        Text("Genre: ${movie.genre}")
+        Text("Actors: ${movie.actors}")
+        Text("Rating: ${movie.rating}")
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 6.dp)
+            )
+        Text("Plot: ${movie.plot}")
+
     }
 }
---
+++ app/src/main/java/com/example/movieappmad24/Movie2.kt
@@ -0,0 +1,3 @@
+package com.example.movieappmad24
+
+data class Movie2(val title: String, val image: Int)
